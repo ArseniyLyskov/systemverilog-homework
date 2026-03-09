c781b6166e780913267eb13c7d92eda29494fc57
Arseniy Lyskov <arseniylyskov@gmail.com>
2026-03-09 19:02:18 +0700

    Homework 7: Fifo

--- 07_fifo/07_06_sqrt_formula_pipe_using_circular_buffer/07_06_formula_pipe_using_circular_buffer.sv
+++ 07_fifo/07_06_sqrt_formula_pipe_using_circular_buffer/07_06_formula_pipe_using_circular_buffer.sv
@@ -42,5 +42,111 @@ module formula_2_pipe_using_circular
     // FPGA-Systems Magazine :: FSM :: Issue ALFA (state_0)
     // You can download this issue from https://fpga-systems.ru/fsm#state_0
 
+    localparam N_PIPE_STAGES = 16;
+
+    logic        isqrt1_down_vld;
+    logic [31:0] isqrt1_down_data;
+
+    logic        isqrt2_up_vld;
+    logic [31:0] isqrt2_up_data;
+    logic        isqrt2_down_vld;
+    logic [31:0] isqrt2_down_data;
+
+    logic        isqrt3_up_vld;
+    logic [31:0] isqrt3_up_data;
+
+    logic [31:0] circ_buff1_down_data;
+    logic [31:0] circ_buff2_down_data;
+
+
+    // intermediate calculations
+    always_ff @(posedge clk or posedge rst)
+        if (rst) isqrt2_up_vld <= '0;
+        else     isqrt2_up_vld <= isqrt1_down_vld;
+
+    always_ff @(posedge clk)
+        if (isqrt1_down_vld)
+            isqrt2_up_data <= isqrt1_down_data + circ_buff1_down_data;
+
+    always_ff @(posedge clk or posedge rst)
+        if (rst) isqrt3_up_vld <= '0;
+        else     isqrt3_up_vld <= isqrt2_down_vld;
+
+    always_ff @(posedge clk)
+        if (isqrt2_down_vld)
+            isqrt3_up_data <= isqrt2_down_data + circ_buff2_down_data;
+        
+
+    // circular_buffer 1
+    circular_buffer_with_valid #(
+        .width     ( 32                   ),
+        .depth     ( N_PIPE_STAGES        )
+    ) circular_buffer1 (
+        .clk       ( clk                  ),
+        .rst       ( rst                  ),
+
+        .in_valid  ( arg_vld              ),
+        .in_data   ( b                    ),
+
+        .out_valid (                      ),
+        .out_data  ( circ_buff1_down_data )
+    );
+
+    // circular_buffer 2
+    circular_buffer_with_valid #(
+        .width     ( 32                   ),
+        .depth     ( N_PIPE_STAGES*2 + 1  )
+    ) circular_buffer2 (
+        .clk       ( clk                  ),
+        .rst       ( rst                  ),
+
+        .in_valid  ( arg_vld              ),
+        .in_data   ( a                    ),
+
+        .out_valid (                      ),
+        .out_data  ( circ_buff2_down_data )
+    );
+    
+    // isqrt1  
+    isqrt #(
+        .n_pipe_stages(N_PIPE_STAGES)
+    ) isqrt1 (
+        .clk       ( clk                  ),
+        .rst       ( rst                  ),
+
+        .x_vld     ( arg_vld              ),
+        .x         ( c                    ),
+
+        .y_vld     ( isqrt1_down_vld      ),
+        .y         ( isqrt1_down_data     )
+    );
+
+    // isqrt2
+    isqrt #(
+        .n_pipe_stages(N_PIPE_STAGES)
+    ) isqrt2 (
+        .clk       ( clk                  ),
+        .rst       ( rst                  ),
+
+        .x_vld     ( isqrt2_up_vld        ),
+        .x         ( isqrt2_up_data       ),
+
+        .y_vld     ( isqrt2_down_vld      ),
+        .y         ( isqrt2_down_data     )
+    );
+    
+    // isqrt3
+    isqrt #(
+        .n_pipe_stages(N_PIPE_STAGES)
+    ) isqrt3 (
+        .clk       ( clk                  ),
+        .rst       ( rst                  ),
+
+        .x_vld     ( isqrt3_up_vld        ),
+        .x         ( isqrt3_up_data       ),
+
+        .y_vld     ( res_vld              ),
+        .y         ( res                  )
+    );
 
 endmodule
--- 07_fifo/07_07_sqrt_formula_pipe_using_fifo/07_07_formula_2_pipe_using_fifos.sv
+++ 07_fifo/07_07_sqrt_formula_pipe_using_fifo/07_07_formula_2_pipe_using_fifos.sv
@@ -41,5 +41,116 @@ module formula_2_pipe_using_fifos
     // FPGA-Systems Magazine :: FSM :: Issue ALFA (state_0)
     // You can download this issue from https://fpga-systems.ru/fsm
 
+    localparam N_PIPE_STAGES = 16;
+
+    logic        isqrt1_down_vld;
+    logic [31:0] isqrt1_down_data;
+
+    logic        isqrt2_up_vld;
+    logic [31:0] isqrt2_up_data;
+    logic        isqrt2_down_vld;
+    logic [31:0] isqrt2_down_data;
+
+    logic        isqrt3_up_vld;
+    logic [31:0] isqrt3_up_data;
+
+    logic [31:0] fifo1_down_data;
+    logic [31:0] fifo2_down_data;
+
+
+    // intermediate calculations
+    always_ff @(posedge clk or posedge rst)
+        if (rst) isqrt2_up_vld <= '0;
+        else     isqrt2_up_vld <= isqrt1_down_vld;
+
+    always_ff @(posedge clk)
+        if (isqrt1_down_vld)
+            isqrt2_up_data <= isqrt1_down_data + fifo1_down_data;
+
+    always_ff @(posedge clk or posedge rst)
+        if (rst) isqrt3_up_vld <= '0;
+        else     isqrt3_up_vld <= isqrt2_down_vld;
+
+    always_ff @(posedge clk)
+        if (isqrt2_down_vld)
+            isqrt3_up_data <= isqrt2_down_data + fifo2_down_data;
+        
+
+    // fifo 1
+    flip_flop_fifo_with_counter #(
+        .width      ( 32                  ),
+        .depth      ( N_PIPE_STAGES       )
+    ) fifo1 (
+        .clk        ( clk                 ),
+        .rst        ( rst                 ),
+
+        .push       ( arg_vld             ),
+        .pop        ( isqrt1_down_vld     ),
+        .write_data ( b                   ),
+
+        .read_data  ( fifo1_down_data     ),
+        .empty      (                     ),
+        .full       (                     )
+    );
+
+    // fifo 2
+    flip_flop_fifo_with_counter #(
+        .width      ( 32                  ),
+        .depth      ( N_PIPE_STAGES*2 + 1 )
+    ) fifo2 (
+        .clk        ( clk                 ),
+        .rst        ( rst                 ),
+
+        .push       ( arg_vld             ),
+        .pop        ( isqrt2_down_vld     ),
+        .write_data ( a                   ),
+
+        .read_data  ( fifo2_down_data     ),
+        .empty      (                     ),
+        .full       (                     )
+    );
+
+    
+    // isqrt1  
+    isqrt #(
+        .n_pipe_stages(N_PIPE_STAGES)
+    ) isqrt1 (
+        .clk       ( clk                  ),
+        .rst       ( rst                  ),
+
+        .x_vld     ( arg_vld              ),
+        .x         ( c                    ),
+
+        .y_vld     ( isqrt1_down_vld      ),
+        .y         ( isqrt1_down_data     )
+    );
+
+    // isqrt2
+    isqrt #(
+        .n_pipe_stages(N_PIPE_STAGES)
+    ) isqrt2 (
+        .clk       ( clk                  ),
+        .rst       ( rst                  ),
+
+        .x_vld     ( isqrt2_up_vld        ),
+        .x         ( isqrt2_up_data       ),
+
+        .y_vld     ( isqrt2_down_vld      ),
+        .y         ( isqrt2_down_data     )
+    );
+    
+    // isqrt3
+    isqrt #(
+        .n_pipe_stages(N_PIPE_STAGES)
+    ) isqrt3 (
+        .clk       ( clk                  ),
+        .rst       ( rst                  ),
+
+        .x_vld     ( isqrt3_up_vld        ),
+        .x         ( isqrt3_up_data       ),
+
+        .y_vld     ( res_vld              ),
+        .y         ( res                  )
+    );
 
 endmodule
